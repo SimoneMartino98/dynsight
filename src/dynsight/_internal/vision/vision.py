bac9327903f2b883341161f5e11916f9e01c21b4
SimoneMartino98 <s.martino0898@gmail.com>
2025-04-07 10:05:49 +0200

    fix dict

--- src/dynsight/_internal/vision/vision.py
+++ src/dynsight/_internal/vision/vision.py
@@ -57,7 +57,7 @@ def train_model(img_path: pathlib.Path) -> None:
     app = LabelCreator(root, img_path)
     root.mainloop()
     res = app.get_boxes()
-    for _, val in res.values():
+    for val in res.values():
         print(
             f"{val['center_x']} {val['center_y']} {val['width']} {val['height']}"
         )
