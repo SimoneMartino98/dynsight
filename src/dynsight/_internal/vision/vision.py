3bb129f3762da5f9a6d3cf0cf0c0b45ac028e301
SimoneMartino98 <s.martino0898@gmail.com>
2025-04-07 14:03:21 +0200

    added training on guess

--- src/dynsight/_internal/vision/vision.py
+++ src/dynsight/_internal/vision/vision.py
@@ -8,6 +8,7 @@ if TYPE_CHECKING:
 from pathlib import Path
 
 import cv2
+from ultralytics import YOLO
 
 from .label_creator import LabelCreator
 
@@ -82,3 +83,26 @@ def train_model(img_path: pathlib.Path) -> None:
             f"nc: 1\n"
             f"names: ['object']\n"
         )
+    # Initial training
+    model = YOLO("yolo12x.pt")
+    model.train(
+        data=yaml_file,  # Path to the dataset configuration file
+        epochs=100,  # Number of training epochs
+        imgsz=1080,  # Image size
+        batch=2,  # Batch size (adjust based on GPU memory)
+        lr0=0.001,  # Initial learning rate
+        lrf=0.01,  # Final learning rate (scheduler)
+        optimizer="auto",  # Use Stochastic Gradient Descent (try 'Adam' too)
+        augment=True,  # Enable augmentations
+        fliplr=0.5,  # Horizontal flip probability
+        flipud=0.5,  # Vertical flip probability
+        hsv_h=0.015,  # Adjust hue
+        hsv_s=0.7,  # Adjust saturation
+        hsv_v=0.4,  # Adjust brightness
+        mosaic=0.5,  # Enable mosaic augmentation
+        mixup=0.0,  # MixUp augmentation
+        device=[2, 3],  # Use multiple GPUs
+        patience=10,  # Early stopping patience
+        workers=16,  # Number of workers for data loading
+        name="pretrained_model",  # Name for the training run
+    )
