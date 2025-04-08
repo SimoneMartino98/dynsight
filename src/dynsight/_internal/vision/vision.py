5f83be2ed3983aa3d55bc19c321828dcc913ba53
SimoneMartino98 <s.martino0898@gmail.com>
2025-04-08 14:51:11 +0200

    debug

--- src/dynsight/_internal/vision/vision.py
+++ src/dynsight/_internal/vision/vision.py
@@ -8,6 +8,8 @@ if TYPE_CHECKING:
 import cv2
 from ultralytics import YOLO
 
+from .label_creator import create_dataset
+
 
 def extract_frames(
     video_path: pathlib.Path,
@@ -54,7 +56,7 @@ def train_model(
     val_img_path: pathlib.Path,
     yaml_file: pathlib.Path,
 ) -> None:
-    # create_dataset(train_img_path, val_img_path)
+    create_dataset(train_img_path, val_img_path)
 
     # Initial training
     model = YOLO("yolo12x.pt")
@@ -75,7 +77,7 @@ def train_model(
         mosaic=0.5,  # Enable mosaic augmentation
         mixup=0.0,  # MixUp augmentation
         device=[2, 3],  # Use multiple GPUs
-        patience=50,  # Early stopping patience
+        patience=10,  # Early stopping patience
         workers=16,  # Number of workers for data loading
         name="pretrained_model",  # Name for the training run
     )
